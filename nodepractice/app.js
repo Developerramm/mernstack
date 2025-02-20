49b261ab0bf6f502593255d2b34ae519fc3f12fe
ram kumar <rr697144@gmail.com>
2025-02-20 15:27:46 +0530

    exports property in node.js

--- nodepractice/app.js
+++ nodepractice/app.js
@@ -1,3 +1,9 @@
-const add = require("./math");
+const { add, subs, multiplication, division } = require("./math");
 
 console.log(add(5, 10));
+console.log(subs(20, 10));
+console.log(multiplication(10, 10));
+console.log(division(50, 3));
+
+const pi = Math.PI;
+console.log(pi)
--- nodepractice/math.js
+++ nodepractice/math.js
@@ -2,4 +2,21 @@ const add = (a, b) => {
   return a + b;
 };
 
-module.exports = add;
+const subs = (a, b) => {
+  return a - b;
+};
+
+const multiplication = (a, b) => {
+  return a * b;
+};
+
+const division = (a, b) => {
+  return a / b;
+};
+
+module.exports = {
+  add,
+  subs,
+  multiplication,
+  division,
+};
