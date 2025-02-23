f44a2fa74926d05cb939b28a349d253309c87011
ram kumar <rr697144@gmail.com>
2025-02-23 07:09:19 +0530

    react app setup

--- nodepractice/httpserver/server.js
+++ nodepractice/httpserver/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
-    res.write("<h1>Welcome to home page official here </h1>");
+    res.write("<h1>Welcome to home page  </h1>");
     res.end();
   }
 
---
+++ react/demo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App here </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nam odio
+        deleniti aperiam in fugit distinctio quae asperiores qui nesciunt?
+      </p>
+    </div>
+  );
+};
+
+export default App;
