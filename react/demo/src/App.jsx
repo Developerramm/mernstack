5609fa1d3ce9a25fb565ca5fb9406038baf30715
ram kumar <rr697144@gmail.com>
2025-02-23 11:59:43 +0530

    form data handle using react router dom

--- react/demo/src/App.jsx
+++ react/demo/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
 import Home from "./components/UI/pages/Home";
 import About from "./components/UI/pages/About";
-import Contact from "./components/UI/pages/Contact";
+import Contact, { contactData } from "./components/UI/pages/Contact";
 import Product from "./components/UI/pages/Product";
 import Movie from "./components/UI/pages/Movie";
 import GetApiData from "./components/api/GetApiData";
@@ -27,21 +27,22 @@ const App = () => {
         {
           path: "/contact",
           element: <Contact />,
+          action: contactData,
         },
         {
           path: "/product",
           element: <Product />,
         },
         {
-          path : "/movie",
-          element : <Movie />,
-          loader : GetApiData,
+          path: "/movie",
+          element: <Movie />,
+          loader: GetApiData,
         },
         {
-          path : "/movie/:id",
-          element : <DetailsPage />,
-          loader : GetDataById,
-        }
+          path: "/movie/:id",
+          element: <DetailsPage />,
+          loader: GetDataById,
+        },
       ],
     },
   ]);
--- react/demo/src/components/UI/pages/Contact.jsx
+++ react/demo/src/components/UI/pages/Contact.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Form } from "react-router-dom";
 
+export const contactData = async ({ request }) => {
+  try {
+    const response = await request.formData();
+    const data = Object.fromEntries(response);
+    console.log(data);
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
+};
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact page </h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore unde dolorum culpa quas adipisci error, quo magni, fuga dicta nobis distinctio! Distinctio nihil esse, vero incidunt possimus ex dolores odio?</p>
+    <div className="row">
+      <div className="col">
+        <h3 className="text-center my-3">Contact Form</h3>
+        <Form method="POST" action="/contact">
+          <div className="mb-3">
+            <label className="form-label">
+              {" "}
+              <b>Name : </b>{" "}
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">
+              {" "}
+              <b>Email : </b>{" "}
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">
+              {" "}
+              <b>Password: </b>{" "}
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              autoComplete="off"
+            />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        </Form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
