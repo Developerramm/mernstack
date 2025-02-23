6206ad06ff197659059eeb27b89c8024397568fd
ram kumar <rr697144@gmail.com>
2025-02-23 10:13:39 +0530

    use universal loading

--- react/demo/src/components/UI/pages/Movie.jsx
+++ react/demo/src/components/UI/pages/Movie.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-
 const Movie = () => {
   const movieData = useLoaderData();
-  // console.log(movieData.Search);
   const arr = movieData.Search;
-  console.log(arr);
   return (
-    <div className="row">
-      {arr.map((item, index) => {
+    <div className="row mt-4">
+      {movieData && arr.map((item, index) => {
         return (
-          <div className="col-md-3" key={index}>
-            <div className="card text-center" style={{ width: "18rem" }}>
-              <img src={item.Poster} className="card-img-top" />
+          <div className="col-md-3 border border-1 text-center" key={index}>
+            <div className="card text-center" style={{ width: "14rem" }}>
+              <img src={item.Poster} className="card-img-top"  />
             </div>
             <div className="card-body">
               <p className="card-title text-center"> Titlt : {item.Title} </p>
--- react/demo/src/components/layout/AppLayout.jsx
+++ react/demo/src/components/layout/AppLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../UI/pages/Header";
 import Footer from "../UI/pages/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const AppLayout = () => {
+  const navigation =  useNavigation();
+  if(navigation.state === "loading") return <h2>Loading....</h2>
   return (
     <div>
       <Header />
