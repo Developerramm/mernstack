5dbd94ce99240518f3cc0b02c948d18fa177cce4
ram kumar <rr697144@gmail.com>
2025-02-24 10:59:19 +0530

    react axios fetch data

--- react/react_axios/src/App.jsx
+++ react/react_axios/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getData } from "./api/PostApi";
 
 const App = () => {
+
+  const [data,setData] = useEffect();
+
+  const getPostData = async()=>{
+    const res = await getData();
+    console.log(res.data)
+    // setData(res.data);
+  }
+
+  useEffect(()=>{
+    getPostData();
+  },[])
+
+
   return (
     <div className="container">
-      <h1>React App </h1>
+      <h1 className="text-center">Crud operation here  </h1>
     </div>
   );
 };
